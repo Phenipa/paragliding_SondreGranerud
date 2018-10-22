00c27bcfd0e450b41e6c56e3709ead4a827be7f6
Phenipa <sondrgra@stud.ntnu.no>
2018-10-22 16:03:29 +0200

    added id-generation

---
+++ id.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/globalsign/mgo"
+	"github.com/segmentio/ksuid"
+)
+
+func genUniqueID() string { //This function generates a unique id for all new POST-requests. It is recursive to ensure that if an existing id is generated again it won't use it, but rather generate a new one
+	session, err := mgo.Dial(os.Getenv("DBURL"))
+	if err != nil {
+		log.Fatal("Database.connection could not be made: ", err)
+		return ""
+	}
+	c := session.DB("paragliding_igc").C("tracks")
+	id := ksuid.New() //Generate an id
+	var result jsonTrack
+	c.Find(bson.M{"id": id.String()}).One(&result)
+	if result.ID == "" { //Check that the id does not alread exist in the database
+		return id.String() //If not in database, return id
+	}
+	return genUniqueID() //If already in databse, run once more to generate a new id
+}
