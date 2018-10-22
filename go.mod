0659d3d2f82053fc97c7face9c184c69619c752e
Phenipa <sondrgra@stud.ntnu.no>
2018-10-22 16:00:39 +0200

    General fixes

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/julienschmidt/httprouter v1.2.0
 	github.com/marni/goigc v0.1.0
+	github.com/segmentio/ksuid v1.0.2
 )
--- handlers.go
+++ handlers.go
@@ -9,7 +9,6 @@ import (
 	"github.com/globalsign/mgo"
 	"github.com/julienschmidt/httprouter"
 	igc "github.com/marni/goigc"
-	"github.com/globalsign/mgo/bson"
 )
 
 type metaData struct { //Output for /paragliding/api
@@ -25,7 +24,7 @@ type jsonTrack struct { //Helper-struct to appropriately respond with data about
 	GliderID    string  `json:"glider_id"`
 	TrackLength float64 `json:"track_length"`
 	URL         string  `json:"url"`
-	_ID 		bson.ObjectId `json:"id"`
+	ID          string  `json:"id"`
 }
 
 func metaHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -37,7 +36,6 @@ func metaHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 func postTrackHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	if r.Header.Get("Content-Type") != "application/json" { //If request is not of type JSON
 		http.Error(w, http.StatusText(http.StatusBadRequest)+"\nRequest needs JSON body", http.StatusBadRequest) //Respond that the request needs to be correctly formatted
-		return
 	}
 	var track jsonTrack
 	err := json.NewDecoder(r.Body).Decode(&track)
@@ -50,7 +48,6 @@ func postTrackHandler(w http.ResponseWriter, r *http.Request, p httprouter.Param
 	if err != nil {
 		log.Fatal("Track parsing failed: ", err)
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
 	}
 	track.Pilot = trackFile.Pilot
 	track.Hdate = trackFile.Date.String()
@@ -63,35 +60,32 @@ func postTrackHandler(w http.ResponseWriter, r *http.Request, p httprouter.Param
 		}
 	}
 	track.TrackLength = length
-	track._ID = bson.NewObjectId()
-	os.Setenv("DBURL", "mongodb://access:paragl1ding_access@ds237363.mlab.com:37363/paragliding_igc")
+	track.ID = genUniqueID()
 	session, err := mgo.Dial(os.Getenv("DBURL"))
 	if err != nil {
 		log.Fatal("Database-connection could not be made: ", err)
+		return
 	}
 	c := session.DB("paragliding_igc").C("tracks")
 	err = c.Insert(track)
-	if err!= nil {
+	if err != nil {
 		log.Fatal("Track could not be inserted: ", err)
-		return
 	}
 	session.Close()
-	json.NewEncoder(w).Encode(track._ID)
+	json.NewEncoder(w).Encode(track.ID)
 }
 
 func getTracklistHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	os.Setenv("DBURL", "mongodb://access:paragl1ding_access@ds237363.mlab.com:37363/paragliding_igc")
 	session, err := mgo.Dial(os.Getenv("DBURL"))
 	if err != nil {
 		log.Fatal("Database-connection could not be made: ", err)
 		return
 	}
 	c := session.DB("paragliding_igc").C("tracks")
-	var indexes []jsonTrack
+	var indexes jsonTrack
 	err = c.Find(nil).All(&indexes)
 	if err != nil {
 		log.Fatal("Could not find indexes: ", err)
-		return
 	}
 	session.Close()
 	json.NewEncoder(w).Encode(indexes)
